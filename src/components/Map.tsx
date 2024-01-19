21b985cb43216a3f41cc8d85162ad58b8bacf22b
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-01-19 15:43:26 +0900

    ch14/ch15 작업
    ch14-식당 상세페이지 구현
    ch15-맛집 목록에서 검색 & 정렬기능

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -11,17 +11,25 @@ declare global {
 const DEFAULT_LAT = 37.497625203;
 const DEFAULT_LNG = 127.03088379;
 
+const DEFAULT_ZOOM = 3;
+
 interface MapProps {
   setMap: Dispatch<SetStateAction<any>>;
+  lat?: string | null;
+  lng?: string | null;
+  zoom?: number;
 }
 
-export default function Map({ setMap }: MapProps) {
+export default function Map({ setMap, lat, lng, zoom }: MapProps) {
   const loadKakaoMap = () => {
     window.kakao.maps.load(() => {
       const mapContainer = document.getElementById("map");
       const mapOption = {
-        center: new window.kakao.maps.LatLng(DEFAULT_LAT, DEFAULT_LNG),
-        level: 3,
+        center: new window.kakao.maps.LatLng(
+          lat ?? DEFAULT_LAT,
+          lng ?? DEFAULT_LNG
+        ),
+        level: zoom ?? DEFAULT_ZOOM,
       };
       const map = new window.kakao.maps.Map(mapContainer, mapOption);
 
@@ -36,7 +44,7 @@ export default function Map({ setMap }: MapProps) {
         src={`//dapi.kakao.com/v2/maps/sdk.js?appkey=${process.env.NEXT_PUBLIC_KAKAO_MAP_CLIENT}&autoload=false`}
         onReady={loadKakaoMap}
       />
-      <div id="map" className="w-full h-screen top-[52px]"></div>
+      <div id="map" className="w-full h-screen"></div>
     </>
   );
 }
--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -2,25 +2,32 @@ import type { NextApiRequest, NextApiResponse } from "next";
 import { StoreApiResponse, StoreType } from "@/interface";
 import { PrismaClient } from "@prisma/client";
 
+interface Responsetype {
+  page?: string;
+  limit?: string;
+  q?: string;
+  district?: string;
+}
+
 export default async function handler(
   req: NextApiRequest,
-  res: NextApiResponse<StoreApiResponse | StoreType[]>
+  res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType>
 ) {
-
-  const { page = "" }: { page?: string } = req.query;
+  const { page = "", limit = "", q, district }: Responsetype = req.query;
   const prisma = new PrismaClient();
 
-  if(page) {
+  if (page) {
     const count = await prisma.store.count();
-    const skipPage = parseInt(page) -1;
-
+    const skipPage = parseInt(page) - 1;
     const stores = await prisma.store.findMany({
       orderBy: { id: "asc" },
-      take: 10,
+      where: {
+        name: q ? { contains: q } : {},
+        address: district ? { contains: district } : {},
+      },
+      take: parseInt(limit),
       skip: skipPage * 10,
     });
-
-    // totalpage, data, page
 
     res.status(200).json({
       page: parseInt(page),
@@ -29,10 +36,15 @@ export default async function handler(
       totalPage: Math.ceil(count / 10),
     });
   } else {
+    const { id }: { id?: string } = req.query;
+
     const stores = await prisma.store.findMany({
       orderBy: { id: "asc" },
+      where: {
+        id: id ? parseInt(id) : {},
+      },
     });
 
-    return res.status(200).json(stores);
+    return res.status(200).json(id ? stores[0] : stores);
   }
 }
