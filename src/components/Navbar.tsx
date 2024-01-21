ed103870c2759a2f40a1d51dd276756765afad0b
GOBORAM <109337202+g-boram@users.noreply.github.com>
2024-01-21 14:38:24 +0900

    ch21 / ch22
    (1) ch21-마이페이지 작업
    (2) ch22-댓글기능 / 마이페이지에서 내가쓴 댓글 모아보기 작업

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -4,7 +4,6 @@ import { useSession, signOut } from "next-auth/react";
 
 import { BiMenu } from "react-icons/bi";
 import { AiOutlineClose } from "react-icons/ai";
-import { sign } from "crypto";
 
 export default function Navbar() {
   const [isOpen, setIsOpen] = useState(false);
@@ -17,9 +16,6 @@ export default function Navbar() {
           nextmap
         </Link>
         <div className="navbar__list">
-          <Link href="/" className="navbar__list--item">
-            맛집 지도
-          </Link>
           <Link href="/stores" className="navbar__list--item">
             맛집 목록
           </Link>
@@ -28,6 +24,9 @@ export default function Navbar() {
           </Link>
           <Link href="/users/likes" className="navbar__list--item">
             찜한 가게
+          </Link>
+          <Link href="/users/mypage" className="navbar__list--item">
+            마이페이지
           </Link>
           {status === "authenticated" ? (
             <button type="button" onClick={() => signOut()}>
@@ -52,21 +51,54 @@ export default function Navbar() {
       {isOpen && (
         <div className="navbar--mobile">
           <div className="navbar__list--mobile">
-            <Link href="/stores" className="navbar__list--item--mobile">
+            <Link
+              href="/stores"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               맛집 목록
             </Link>
-            <Link href="/stores/new" className="navbar__list--item--mobile">
+            <Link
+              href="/stores/new"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               맛집 등록
             </Link>
-            <Link href="/users/likes" className="navbar__list--item--mobile">
+            <Link
+              href="/users/likes"
+              className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
+            >
               찜한 가게
             </Link>
             <Link
-              href="/api/auth/signin"
+              href="/users/mypage"
               className="navbar__list--item--mobile"
+              onClick={() => setIsOpen(false)}
             >
-              로그인
+              마이페이지
             </Link>
+            {status === "authenticated" ? (
+              <button
+                type="button"
+                onClick={() => {
+                  signOut();
+                  setIsOpen(false);
+                }}
+                className="navbar__list--item--mobile text-left"
+              >
+                로그아웃
+              </button>
+            ) : (
+              <Link
+                href="/api/auth/signin"
+                className="navbar__list--item--mobile"
+                onClick={() => setIsOpen(false)}
+              >
+                로그인
+              </Link>
+            )}
           </div>
         </div>
       )}
